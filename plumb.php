477517954fdbd8004a63e6c6e4721695ff7af347
Chris Warburton <chriswarbo@gmail.com>
2014-08-20 03:06:32 +0100

    Use core

--- plumb.php
+++ plumb.php
@@ -1,55 +1,35 @@
 <?php
 
-namespace {
-  // Grouping syntax
-  function __() { return array_merge(['grouped' => true], func_get_args()); }
+// Grouping syntax
+function __() { return array_merge(['grouped' => true], func_get_args()); }
+
+call_user_func(function() {
+  $interpret = null;
+
+  $call = curry(function(array $env, $f, $arg) use (&$interpret) {
+                  return op($f, $interpret($env, $arg));
+                });
+
+  $chain = curry(function(array $env, array $calls) use ($call) {
+                   return array_reduce(
+                            array_diff_key($calls, ['grouped' => '']),
+                            $call($env),
+                            function($x) { return $x; });
+                 });
+
+  $plumb = curry(function(array $env, array $expr, $arg) use ($chain) {
+                   return $chain(array_merge([$arg], $env), $expr);
+                 });
+
+  $interpret = curry(function(array $e, $x) use ($chain, $plumb) {
+                       // Assume ints and arrays are Plumb; anything else is PHP
+                       if (is_int  ($x)) return $e[$x];
+                       if (is_array($x)) return isset($x['grouped'])
+                                                  ? $chain($e, $x)
+                                                  : $plumb($e, $x);
+                       return $x;
+  });
 
   // Interpret $f in an empty environment
-  function plumb($f) { return Plumb\Internal\plumb([], $f); }
-}
-
-namespace Plumb\Internal {
-
-  function _curry($n, $args, $f) { return function() use ($n, $args, $f) {
-    $all_args = array_merge($args, func_get_args());
-    return (count($all_args) < $n)
-      ?  _curry($n, $all_args, $f)
-      : array_reduce(array_slice($all_args, $n),
-                    'call_user_func',
-                     call_user_func_array($f, array_slice($all_args, 0, $n)));
-  }; }
-
-  function curry($n, $f) { return _curry($n, [], $f); }
-
-  function plumb(array $env, array $expr) { return function($arg) use ($env, $expr) {
-    return chain(array_merge([$arg], $env), $return_expr);
-  }; }
-
-  function chain(array $env, array $calls) {
-    return array_reduce(array_diff_key($calls, ['grouped' => '']),
-                        call($env),
-                        function($x) { return $x; });
-  }
-
-  function call(array $env) { return function($func, $arg) use ($env) { return $f(interpret($e, $x)); };
-  }
-
-  function interpret(array $e, $x) {
-    // Assume ints and arrays are Plumb; anything else is PHP
-    if (is_int  ($x)) return int($e, $x);
-    if (is_array($x)) return arr($e, $x);
-    return $x;
-  }
-
-  function int(array $e, $i) {
-    // Look up argument reference $i in environment $e
-    return isset($e[$i])? $e[$i]
-                        : error("No $i at level {$id(count($e))}");
-  }
-
-  function arr(array $e, array $a) {
-    // Chain calls $a; wrap them in a function unless they're grouped
-    return isset($a['grouped'])? chain($e, $a)
-                               : plumb($e, $a);
-  }
-}
+  defun('plumb', $plumb([]));
+});
--- test.php
+++ test.php
@@ -2,26 +2,36 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
+function id($x) { return $x; }
+
 deftests([
-    'id'    => function($n) {
-        return eq(plumb([0], $n), $n)
-        ?: var_dump($n);
-    },
-    'const' => function($n, $m) {
-        return eq(plumb([[1]], $n, $m), $n)
-        ?: var_dump($n, $m);
-    },
-    'comp'  => function ($l, $m, $n) {
-        $lhs = plumb([[[2, _(1, 0)]]], plus($l), mult($m), $n);
-        return eq($lhs, $l + ($m * $n))
-        ?: var_dump(get_defined_vars());
-    },
-    'nest'  => function ($n) {
-        $lhs = plumb([_('plus', _('mult', 0, 0)),
-        _(_('id', 'id'), 0)], $n);
-        return eq($lhs, ($n+1) * $n)
-        ?: var_dump(get_defined_vars());
-    },
+  'id'    => function($n) {
+               $result = plumb([0], $n);
+               return ($result === $n)? 0 : get_defined_vars();
+             },
+
+  'const' => function($n, $m) {
+               $result = plumb([[1]], $n, $m);
+               return ($result === $n)? 0 : get_defined_vars();
+             },
+
+  'comp'  => function ($l, $m, $n) {
+               $lhs = plumb([[[2, __(1, 0)]]],
+                            op('+', $l),
+                            op('*', $m),
+                            $n);
+               $rhs = $l + ($m * $n);
+               return ($lhs === $rhs)? 0 : get_defined_vars();
+             },
+
+  'nest'  => function ($n) {
+               $lhs = plumb([__('+', __('*', 0, 0)),
+                             __(__('id', 'id'), 0)],
+                            $n);
+               $rhs = ($n+1) * $n;
+               return ($lhs === $rhs)? 0 : get_defined_vars();
+             },
 ]);
 
-run_test(null);
+if ($results = runtests(null)) var_dump($results);
+else echo "All tests pass\n";
